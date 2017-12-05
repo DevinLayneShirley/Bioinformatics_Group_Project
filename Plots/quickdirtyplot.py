b8dcbe3cd22aea499ce018891c429e160c5185d3
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-12-05 17:08:17 -0500

    cleaned the project summary up, finished exploration part 1

--- Plots/quickdirtyplot.py
+++ Plots/quickdirtyplot.py
@@ -3,4 +3,3 @@ from plotnine import *
 data=pandas.read_csv("expressionlevels.csv")
 plot=ggplot(data,aes(x='Protein',y='ExpressionLevel',color='Treatment'))
 plot+geom_jitter(width=.1,height=.3,alpha=.5,size=5)+theme_classic()+ggtitle('Protein Expression Levels for Each Treatment')
-?geom_jitter
