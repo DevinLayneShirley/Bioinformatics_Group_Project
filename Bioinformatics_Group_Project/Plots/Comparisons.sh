4f6a01f95c15a6349f6485fab87a46d3db80b5df
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-12-06 11:54:59 -0500

    cleaned up repositories, made extensions the same, except RNA_Seq repo

--- Bioinformatics_Group_Project/Plots/Comparisons.sh
+++
@@ -1,4 +0,0 @@
-grep -v '#' control1.protein.hits.table_rodent | awk '{print $1,$3}' | uniq -c | awk '{print $1,$2,$3}' > Control1Counts.txt
-grep -v '#' control2.protein.hits.table_rodent | awk '{print $1,$3}' | uniq -c | awk '{print $1,$2,$3}' > Control2Counts.txt
-grep -v '#' obese1.protein.hits.table_rodent | awk '{print $1,$3}' | uniq -c | awk '{print $1,$2,$3}' > Obese1Counts.txt
-grep -v '#' obese2.protein.hits.table_rodent | awk '{print $1,$3}' | uniq -c | awk '{print $1,$2,$3}' > Obese2Counts.txt
--- Bioinformatics_Group_Project/Plots/quickdirtyplot.py
+++
@@ -1,6 +0,0 @@
-import pandas
-from plotnine import *
-data=pandas.read_csv("expressionlevels.csv")
-plot=ggplot(data,aes(x='Protein',y='ExpressionLevel',color='Treatment'))
-plot+geom_jitter(width=.1,height=.3,alpha=.5,size=5)+theme_classic()+ggtitle('Protein Expression Levels for Each Treatment')
-?geom_jitter
