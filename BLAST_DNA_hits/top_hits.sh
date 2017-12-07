c33bf38c4f2592be11bb2d97e0f8dcbca60e1825
Devin Layne Shirley <devin.layne.shirley@gmail.com>
2017-12-07 09:19:30 -0500

    adding comments, fixing project plan, etc.

--- BLAST_DNA_hits/top_hits.sh
+++ BLAST_DNA_hits/top_hits.sh
@@ -1,3 +1,6 @@
+#Reads all of the .csv files in the repository, takes their top line
+#and moves them to a new file
+
 for file in *.csv; do head -1 $file | cat top_hits; done
 
-//Unix code that works when the data is in six different .csv files
+
